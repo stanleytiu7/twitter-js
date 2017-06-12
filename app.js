9ad139f022d5bc0965a15fb82a4b9a6f6896ff27
Stanley Tiu <stanleytiu7@gmail.com>
2017-06-12 18:22:06 -0400

    switching roles

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000!');
