5abaca4e3fb0e6aeb3ff46c4bef5871521769257
Denys Andreiev <denisandreev64@gmail.com>
2017-06-13 11:13:14 -0400

    workshop commit

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+//const router = express.Router();
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
+});
+
+app.use((req, res, next) => {
+  const user = req.query.user;
+  console.log('user',user, 'at ip',req.ip);
+  //console.log(router.route);
+  next();
+});
+
+app.get('/', (req, res) => {
+  console.log('GET /')
+  res.send('Hello world!');});
+
+app.get('/game', (req, res) => {
+  console.log('GET /game')
+  res.redirect('https://denisual.github.io/game-of-life.1706/');});
